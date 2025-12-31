6cc0d0cef2e3ee0d9975c5002e27b92682ad3dc6
miqisu <miqi.su@gmail.com>
2025-12-31 11:30:26 +0800

    refactor: remove some unnecessary comment

--- web_extraction.py
+++ web_extraction.py
@@ -44,13 +44,6 @@ def get_data_from_html(html):
     first_elem = temp[1]
     temp_split = first_elem.split("<")
     scientific_name = temp_split[0]
-    
-    # temp = html.split("sciName:\"")
-    # first_elem = temp[1]
-    # temp_split = first_elem.split("\"")
-    # scientific_name = temp_split[0]
-
-    # <span class="Heading-sub Heading-sub--inline Heading-sub--sci">Rostrhamus sociabilis</span>
 
     # Get photographer name
     temp = html.split("© ")
