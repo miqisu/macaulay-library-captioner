463636ced365ba8c9de1d89c8c335b50df88cab9
miqisu <miqi.su@gmail.com>
2025-12-31 15:49:56 +0800

    feat: added caption_txt_of_images, add captions to the images in every link in a txt file of according format

--- captioner.py
+++ captioner.py
@@ -2,6 +2,7 @@ import urllib.request
 from PIL import Image, ImageFont, ImageDraw
 import requests
 from io import BytesIO
+import os 
 
 import web_extraction
 import image_editor
@@ -22,10 +23,32 @@ def caption_image(link):
     return captioned_img
 
 # Caption every image in a txt of links and save to a directory
+# Format must be: One link on each line, no spaces
 def caption_txt_of_images(textfile):
-    # tba
+    # If directory does not exist, make directory for all images to be saved in
+    if not os.path.exists(DIRECTORY_NAME):
+        os.mkdir(DIRECTORY_NAME)
+    
+    with open(textfile) as file:
+        lines = [line.rstrip() for line in file]
+
+    i = 0
+    num_of_lines = len(lines)
+    while i < num_of_lines:
+        curr_line = lines[i]
+        curr_img = caption_image(curr_line)
+        [curr_html, curr_image_id] = web_extraction.extract_html_from_link(curr_line)
+        curr_img.save(f"{DIRECTORY_NAME}/{curr_image_id}.png", "PNG")
+        
+        if i+1 == num_of_lines:
+            print(f"Link {i+1} out of {num_of_lines} processed into image.")
+        else:
+            print(f"Link {i+1} out of {num_of_lines} processed into image...")
+        
+        i+=1
     return
 
 
-test_img = caption_image("https://macaulaylibrary.org/asset/204972611")
-test_img.show()
+caption_txt_of_images("test.txt")
+# test_img = caption_image("https://macaulaylibrary.org/asset/204972611")
+# test_img.show()
